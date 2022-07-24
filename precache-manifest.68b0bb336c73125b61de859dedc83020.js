d243a05b53d60edac5c2a63f043ef2cf54b077d9
koeunchorong <koasis03@gmail.com>
2022-07-24 20:17:36 +0900

    Updates

---
+++ precache-manifest.68b0bb336c73125b61de859dedc83020.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "59a79c54525e03ff41e8",
+    "url": "/MusicStone_Client/static/css/main.155b2fce.chunk.css"
+  },
+  {
+    "revision": "59a79c54525e03ff41e8",
+    "url": "/MusicStone_Client/static/js/main.53ea5c30.chunk.js"
+  },
+  {
+    "revision": "aaae86ed42357114831a",
+    "url": "/MusicStone_Client/static/js/runtime~main.9947eb3d.js"
+  },
+  {
+    "revision": "54796bc7aaa142a559df",
+    "url": "/MusicStone_Client/static/css/2.d5f60867.chunk.css"
+  },
+  {
+    "revision": "54796bc7aaa142a559df",
+    "url": "/MusicStone_Client/static/js/2.7e372632.chunk.js"
+  },
+  {
+    "revision": "78bf8c8f3f42eefa8e747c93a8946130",
+    "url": "/MusicStone_Client/static/media/musicstonelogo.78bf8c8f.png"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MusicStone_Client/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MusicStone_Client/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MusicStone_Client/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MusicStone_Client/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "17f36e2089c769accb47eaf425745e67",
+    "url": "/MusicStone_Client/static/media/비트로 코어 TTF.17f36e20.ttf"
+  },
+  {
+    "revision": "15409c9430a3931f842230b28b0752f5",
+    "url": "/MusicStone_Client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MusicStone_Client/main/precache-manifest.bb52b4034fac2e14b26610d500f2a2ab.js"
+  "/MusicStone_Client/precache-manifest.68b0bb336c73125b61de859dedc83020.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MusicStone_Client/main/index.html", {
+workbox.routing.registerNavigationRoute("/MusicStone_Client/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
