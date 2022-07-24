1c01294c4455943b5cbdb025d27dd44059ad8585
koeunchorong <koasis03@gmail.com>
2022-07-24 19:47:47 +0900

    Updates

---
+++ precache-manifest.ec586e44ba28fb6f43e9dfc8c3aec9ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec62fcb1603ff601fd2",
+    "url": "/MusicStone_Client/static/css/main.f3c81ab3.chunk.css"
+  },
+  {
+    "revision": "aec62fcb1603ff601fd2",
+    "url": "/MusicStone_Client/static/js/main.1cfec062.chunk.js"
+  },
+  {
+    "revision": "aaae86ed42357114831a",
+    "url": "/MusicStone_Client/static/js/runtime~main.9947eb3d.js"
+  },
+  {
+    "revision": "54796bc7aaa142a559df",
+    "url": "/MusicStone_Client/static/css/2.d5f60867.chunk.css"
+  },
+  {
+    "revision": "54796bc7aaa142a559df",
+    "url": "/MusicStone_Client/static/js/2.7e372632.chunk.js"
+  },
+  {
+    "revision": "78bf8c8f3f42eefa8e747c93a8946130",
+    "url": "/MusicStone_Client/static/media/musicstonelogo.78bf8c8f.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MusicStone_Client/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MusicStone_Client/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MusicStone_Client/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MusicStone_Client/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "17f36e2089c769accb47eaf425745e67",
+    "url": "/MusicStone_Client/static/media/비트로 코어 TTF.17f36e20.ttf"
+  },
+  {
+    "revision": "48612fb6f3d31847be383a830274179b",
+    "url": "/MusicStone_Client/index.html"
+  }
+];
